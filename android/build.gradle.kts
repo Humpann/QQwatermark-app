1a15368757daf55e1694863187f633f0f02744ec
Humpann <Humpann@users.noreply.github.com>
2026-08-24 16:44:46 +0800

    fix(vercel): clean python dependencies and configure explicit @vercel/python build

--- android/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
--- android/打开AndroidStudio工程.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo 正在打开 Android Studio 并导入 OmniMediaWatermarkApp 工程...
-start "" "C:\Program Files\Android\Android Studio\bin\studio64.exe" "%~dp0."
-exit
